de3b4c3cc7294b2bd12204e4e3fefd367fa3a34f
Sergio Garcia <sergioa.garcia217@gmail.com>
2016-11-14 15:05:43 -0500

    Add files via upload

---
+++ Assets/Player.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var speed:float; 
+
+function Start () {
+
+}
+
+function Update () {
+    var H = Input.GetAxis("Horizontal"); 
+    var V = Input.GetAxis("Veritical");
+
+    var Movement = Vector3(H,0,V);
+
+transform.Translate (Movement * speed * Time.deltaTime);
+
+
+}
