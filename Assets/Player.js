3fa8cbea73a5f484c2bd1f5eb9cb186c98f862e1
Sergio Garcia <sergioa.garcia217@gmail.com>
2016-11-22 14:51:47 -0500

    Add files via upload

--- Assets/Player.js
+++ Assets/Player.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
-var speed:float; 
+var speed: float;
+var Jump: float;
 
 function Start () {
 
@@ -8,11 +9,13 @@ function Start () {
 
 function Update () {
     var H = Input.GetAxis("Horizontal"); 
-    var V = Input.GetAxis("Veritical");
+    var V = Input.GetAxis("Vertical");
 
     var Movement = Vector3(H,0,V);
 
-transform.Translate (Movement * speed * Time.deltaTime);
+	transform.Translate (Movement * speed * Time.deltaTime);
 
-
+	if(Input.GetKeyDown(KeyCode.Space)){
+		GetComponent.<Rigidbody>().AddForce(Vector3(0,Jump,0));
+	}
 }
