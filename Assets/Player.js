ce15797350dc0e3445a49c742c452f145ffd9c50
Sergio Garcia <sergioa.garcia217@gmail.com>
2016-12-05 15:56:26 -0500

    Add files via upload

--- Assets/Player.js
+++ Assets/Player.js
@@ -2,12 +2,23 @@
 
 var speed: float;
 var Jump: float;
+var grounded:boolean;
+var end: Transform; 
+var Dis: UI.Text;
+var win: GameObject;
 
 function Start () {
 
 }
 
 function Update () {
+	var dist = Vector3.Distance(end.position, transform.position);
+	Dis.text = dist.ToString("F0");
+
+	if(dist<2){
+		win.SetActive(true);	
+	}
+
     var H = Input.GetAxis("Horizontal"); 
     var V = Input.GetAxis("Vertical");
 
@@ -16,6 +27,18 @@ function Update () {
 	transform.Translate (Movement * speed * Time.deltaTime);
 
 	if(Input.GetKeyDown(KeyCode.Space)){
-		GetComponent.<Rigidbody>().AddForce(Vector3(0,Jump,0));
+		if(grounded){
+			grounded=false;
+			GetComponent.<Rigidbody>().AddForce(Vector3(0,Jump,0));
+		}
 	}
+
+	if(transform.position.y < -10){
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
+
+function OnCollisionStay ()
+{
+	grounded = true;	
 }
